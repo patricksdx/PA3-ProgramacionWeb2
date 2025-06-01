4736ccddbe038a85181916a14a8b8d42c346c1a1
Patrick <patrick74966586@gmail.com>
2025-05-31 23:52:14 -0500

    Agregadas dependencias para livereload y configurado el servidor para recarga en caliente. Se actualizó el script de desarrollo y se modificó la vista de contacto.

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import compression from "compression";
+import livereload from "livereload";
+import connectLivereload from "connect-livereload";
 
 import logger from "./config/utils/pino";
 import { corsOptions } from "./config/utils/cors";
@@ -11,6 +13,15 @@ app.use(compression());
 app.use(cors(corsOptions));
 app.use(express.json());
 
+const liveReloadServer = livereload.createServer({
+  exts: ["ejs", "js", "css", "html"],
+  port: 35729,
+});
+liveReloadServer.watch(path.join(__dirname, "./views"));
+liveReloadServer.watch(path.join(__dirname, "../public"));
+
+app.use(connectLivereload());
+
 app.use(express.static(path.join(__dirname, "../public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./views"));
@@ -19,7 +30,11 @@ app.get("/", (req, res) => {
   res.render("index", { mensaje: "Hola desde EJS con TypeScript y Bun!" });
 });
 app.get("/contact", (req, res) => {
-  res.render("contact", { mensaje: "Contacto con ejs con bun" });
+  res.render("contact", { mensaje: "Welcome to jurassic world a" });
+});
+
+liveReloadServer.server.on("change", () => {
+  liveReloadServer.refresh("/");
 });
 
 app.listen(5115, () => {
